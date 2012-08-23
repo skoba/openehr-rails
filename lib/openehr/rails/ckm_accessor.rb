93ae6ee8d757de9bf9b8d5cc5180028a884c4eae
Shinji KOBAYASHI <skoba@moss.gr.jp>
2012-08-23 22:10:46 +0900

    raise exception when archetype could not be retrieved.

--- lib/openehr/rails/ckm_accessor.rb
+++ lib/openehr/rails/ckm_accessor.rb
@@ -5,7 +5,12 @@ module OpenEHR
     class CKMAccessor
       def self.retrieve(archetype_id)
         ckmws = SOAP::WSDLDriverFactory.new('http://openehr.org/knowledge/services/ArchetypeFinderBean?wsdl').create_rpc_driver
-        ckmws.getArchetypeInADL(archetypeId: archetype_id).m_return
+        adl = ckmws.getArchetypeInADL(archetypeId: archetype_id).m_return
+        unless adl.empty?
+          return adl
+        else
+          raise Exception
+        end
       end
     end
   end
--- spec/openehr/rails/ckm_accessor_spec.rb
+++ spec/openehr/rails/ckm_accessor_spec.rb
@@ -2,11 +2,17 @@ require 'spec_helper'
 require 'openehr/rails/ckm_accessor'
 
 describe OpenEHR::Rails::CKMAccessor do
-  before do
-    @adl = OpenEHR::Rails::CKMAccessor.retrieve('openEHR-EHR-OBSERVATION.blood_pressure.v1') 
+  describe 'retrieve archetype ADL by ID' do
+    before do
+      @adl = OpenEHR::Rails::CKMAccessor.retrieve('openEHR-EHR-OBSERVATION.blood_pressure.v1') 
+    end
+
+    it 'is match archetype' do
+      @adl.should match /archetype \(adl_version=1\.4\)/
+    end
   end
 
-  it 'is match archetype' do
-    @adl.should match /archetype \(adl_version=1\.4\)/
-  end 
+  it 'raise exception when it could not retrieve archetype' do
+    expect { OpenEHR::Rails::CKMAccessor.retrieve('invalid_id') }.to raise_error
+  end
 end
