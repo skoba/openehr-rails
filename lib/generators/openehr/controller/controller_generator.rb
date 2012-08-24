07f085a9b16fa67e801ea5c1e3c742ce134a157f
Shinji KOBAYASHI <skoba@moss.gr.jp>
2012-08-25 01:05:27 +0900

    add spec to controller generator

---
+++ lib/generators/openehr/controller/controller_generator.rb
@@ -0,0 +1,9 @@
+module OpenEHR
+  module Generators
+    class ControllerGenerator < ::Rails::Generators::Base
+      desc <<DESC
+generate controler from template and archetype
+DESC
+    end
+  end
+end
---
+++ spec/generators/openehr/controller/controller_generator_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'generators/openehr/controller/controller_generator'
+
+describe OpenEHR::Generators::ControllerGenerator do
+  it 'retrieve archetype from CKM with --remote option'
+
+  it 'parse archetype if app/archetype with --local option'
+end
