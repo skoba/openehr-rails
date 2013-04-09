88686cad4e8e90912a6dc179848b9d5000c3217c
Shinji KOBAYASHI <skoba@moss.gr.jp>
2013-04-09 22:16:56 +0900

    refactrored

--- lib/generators/openehr/i18n/i18n_generator.rb
+++ lib/generators/openehr/i18n/i18n_generator.rb
@@ -10,14 +10,29 @@ module OpenEHR
         source_root File.expand_path '../templates', __FILE__
 
         def create_i18n_file
-          archetype = OpenEHR::Parser::ADLParser.new(name).parse
-          original_language_code = archetype.original_language.code_string
-          @original_language = { code: original_language_code,
-            text: Locale::Info.get_language(original_language_code).name}
-          @translations = archetype.translations.each_key.map do |key|
+          @original_language = original_language
+          @translations = translations
+          template 'i18n.rb', 'config/initializers/i18n.rb'
+        end
+
+        private
+        def archetype
+          OpenEHR::Parser::ADLParser.new(name).parse
+        end
+
+        def original_language
+          { code: original_language_code,
+            text: Locale::Info.get_language(original_language_code).name }
+        end
+
+        def original_language_code
+          archetype.original_language.code_string
+        end
+
+        def translations
+          archetype.translations.each_key.map do |key|
             { code: key, text: Locale::Info.get_language(key).name }
           end
-          template 'i18n.rb', 'config/initializers/i18n.rb'
         end
       end
     end
